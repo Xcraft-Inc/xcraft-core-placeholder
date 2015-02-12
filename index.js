916e9f6e6f1c1125fae8eed51cd4b4b8e46a1f0b
Mathieu Schroeter <schroeter@epsitec.ch>
2015-02-12 16:10:11 +0100

    Add a trivial module for placeholders handling.

---
+++ index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var holders = {};
+
+exports.set = function (key, value) {
+  holders[key] = value;
+};
+
+exports.inject = function (namespace, data) {
+  Object.keys (holders).forEach (function (placeholder) {
+    var regex = new RegExp ('<' + namespace + '\.' + placeholder + '>', 'g');
+    data = data.replace (regex, holders[placeholder]);
+  });
+  return data;
+};
