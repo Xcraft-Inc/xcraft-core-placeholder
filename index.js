ed0fd5dbc8ccf6b1c6466fceeab541f58f8e8cf4
Mathieu Schroeter <mathieu@schroetersa.ch>
2015-02-21 13:56:37 +0100

    Fix escape character in regex.

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ exports.set = function (key, value) {
 
 exports.inject = function (namespace, data) {
   Object.keys (holders).forEach (function (placeholder) {
-    var regex = new RegExp ('<' + namespace + '\.' + placeholder + '>', 'g');
+    var regex = new RegExp ('<' + namespace + '\\.' + placeholder + '>', 'g');
     data = data.replace (regex, holders[placeholder]);
   });
   return data;
