a3485d2ea0c60957d0a02bed7f61995ada251fe4
Mathieu Schroeter <mathieu@schroetersa.ch>
2015-08-28 13:01:08 +0200

    Add a test for the case of a ph is in a ph.
    ph: placeholder

--- test/index.js
+++ test/index.js
@@ -82,5 +82,20 @@ describe ('xcraft-core-placeholder', function () {
         .inject ('NS1', data);
       ph.inject ('NS2', data).should.be.equal (expected);
     });
+
+    it ('inject a placeholder in a placeholder', function () {
+      var expected = '';
+      expected += 'foobar <NS1.FOOBAR>\n';
+      expected += '<NS1.FOOBAR> foobar <NS1.FOOBAR>\n';
+      expected += 'foobar <NS1.BARFOO>\n';
+      expected += '<NS1.FOOBAR> foobar <NS2.FOOBAR>\n';
+      expected += '<NS2.FOOBAR> foobar\n';
+      expected += 'foobar foobar foobar\n';
+      expected += 'foobar foobar';
+
+      ph.set ('FOO.BAR', '<NS3.BAR.FOO>')
+        .set ('BAR.FOO', 'foobar')
+        .inject ('NS3', data).should.be.equal (expected);
+    });
   });
 });
