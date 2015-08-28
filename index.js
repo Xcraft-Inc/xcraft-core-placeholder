5a6cc2a2a89375fb1cd7c93da194e0c69535252b
Mathieu Schroeter <mathieu@schroetersa.ch>
2015-08-28 12:56:53 +0200

    Add the capability of a placeholder to content an other one.
    Then the placeholders are resolved before use.

--- index.js
+++ index.js
@@ -3,15 +3,43 @@
 
 function Placeholder () {
   this.holders = {};
+  this._isResolved = false;
 }
+
+Placeholder.prototype._resolve = function (namespace) {
+  var self = this;
+
+  Object.keys (self.holders).forEach (function (placeholder) {
+    var regex = new RegExp ('<' + namespace + '\\.([^>]+)>', 'g');
+    var res = null;
+    while ((res = regex.exec (self.holders[placeholder]))) {
+      if (res[1] === placeholder) {
+        break;
+      }
+
+      if (!self.holders.hasOwnProperty (res[1])) {
+        continue;
+      }
+
+      self.holders[placeholder] = self.holders[placeholder].replace (regex, self.holders[res[1]]);
+    }
+  });
+
+  self._isResolved = true;
+};
 
 Placeholder.prototype.set = function (key, value) {
   this.holders[key] = value;
+  this._isResolved  = false;
   return this;
 };
 
 Placeholder.prototype.inject = function (namespace, data) {
   var self = this;
+
+  if (!self._isResolved) {
+    self._resolve (namespace);
+  }
 
   Object.keys (self.holders).forEach (function (placeholder) {
     var regex = new RegExp ('<' + namespace + '\\.' + placeholder + '>', 'g');
@@ -33,6 +61,10 @@ Placeholder.prototype.inject = function (namespace, data) {
 
 Placeholder.prototype.injectFile = function (namespace, fileIn, fileOut) {
   var fs = require ('fs');
+
+  if (!this._isResolved) {
+    this._resolve (namespace);
+  }
 
   var data = fs.readFileSync (fileIn, 'utf8');
   data = this.inject (namespace, data);
